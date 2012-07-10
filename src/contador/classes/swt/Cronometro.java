ae472d20aaab5fe8ae792d2a216cd3f7e4f9ff8c
Carlos Saraiva <carlos.saraiva@msn.com>
2012-07-10 19:02:06 -0300

    Update geral.

--- src/contador/classes/swt/Cronometro.java
+++ src/contador/classes/swt/Cronometro.java
@@ -1,6 +1,8 @@
 package contador.classes.swt;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.KeyAdapter;
+import org.eclipse.swt.events.KeyEvent;
 import org.eclipse.swt.events.MouseAdapter;
 import org.eclipse.swt.events.MouseEvent;
 import org.eclipse.swt.events.VerifyEvent;
@@ -14,7 +16,7 @@ import org.eclipse.wb.swt.SWTResourceManager;
 
 import contador.classes.engine.contador;
 
-public class Cronometro extends Composite implements Runnable {
+public class Cronometro extends Composite {
 
 	private Button btnStart;
 	private TaskItem item;
@@ -22,12 +24,26 @@ public class Cronometro extends Composite implements Runnable {
 	private static contador contador;
 	private Thread thread;
 	private Runnable runnable;
+	private int state;
 
+	/**
+	 * 
+	 * @param parent
+	 * @param style
+	 */
 	public Cronometro(Composite parent, int style) {
-
 		super(parent, style);
-
+		state = 0;
 		text = new Text(this, SWT.RIGHT);
+		text.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if(state == 0 && (e.keyCode == SWT.CR || e.keyCode == SWT.KEYPAD_CR)){
+					state = 1 ;
+					startClock();
+				}
+			}
+		});
 		text.setToolTipText("Digite o valor a ser contado.");
 		text.setFont(SWTResourceManager.getFont("Segoe UI", 62, SWT.NORMAL));
 		text.setBounds(0, 0, 298, 120);
@@ -51,59 +67,56 @@ public class Cronometro extends Composite implements Runnable {
 		btnStart.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseUp(MouseEvent e) {
-				item.setProgressState(SWT.NORMAL);
-				contador = new contador(Integer.parseInt(text.getText()));
-				thread = new Thread(runnable);
-				thread.start();
+				if(state == 0){
+					state = 1;
+					startClock();					
+				}
 			}
 		});
 
 		item = Principal.getTaskBarItem();
 		if (item != null) {
 			item.setProgressState(SWT.INDETERMINATE);
-		}
-
-		setClock();
-		System.out.println(runnable);
+		}		
+		setRunnable();
 	}
 
 	@Override
 	protected void checkSubclass() {
 		// Disable the check that prevents subclassing of SWT components
 	}
-
-	@Override
-	public void run() {
-		// TODO Auto-generated method stub
+	private void startClock(){
+		
+		item.setProgressState(SWT.NORMAL);
+		contador = new contador(Integer.parseInt(text.getText()));
+		thread = new Thread(runnable);
+		thread.start();
 	}
 
-	public void setClock() {
+	private void setRunnable() {
+		
 		runnable = new Runnable() {
 			@Override
 			public void run() {
-
 				try {
 					while (contador.getTempo() >= 0) {
-
 						Display.getDefault().asyncExec(new Runnable() {
 							public void run() {
-
-								contador.setTempo();
-								text.setText(String.valueOf(contador.getTempo()));
-								item.setProgress(contador.getProgresso());
+								if (!thread.isInterrupted()) {
+									contador.setTempo();
+									text.setText(String.valueOf(contador.getTempo()));
+									item.setProgress(contador.getProgresso());
+								}
 								if (contador.getTempo() == 0) {
 									item.setProgressState(SWT.INDETERMINATE);
+									state = 0;
 								}
 							}
 						});
 						Thread.sleep(1000);
 					}
-
 					thread.interrupt();
-
 				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-
 					e.printStackTrace();
 				}
 
